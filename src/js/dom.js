8e7b022bec8348d28819fdc1ade4d26b3fff7aa5
Giulio <gcarbonigiulio@gmail.com>
2026-08-31 12:09:13 +0200

    add div wrapper for input and search btn

---
+++ src/js/dom.js
@@ -0,0 +1,4 @@
+export const searchForm = document.getElementById("search-form");
+export const searchInput = document.getElementById("search");
+export const searchButton = document.querySelector("search-btn");
+export const resultsContainer = document.getElementById("results");
