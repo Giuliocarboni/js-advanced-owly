896339e7086b1cb1f831ef4029bcd6f72df9ea89
Giulio <gcarbonigiulio@gmail.com>
2026-09-03 16:03:38 +0200

    chore: rimuovi api-pseudo.js dal tracciamento

--- src/js/api-pseudo.js
+++
@@ -1,9 +0,0 @@
-/* 
-1. L'utente digita e fa submit
-2. Viene inviata richiesta dei dati relativi al genere a openlibrary
-3. Se la richiesta va buon fine vengono mostrati dati a schermo
-4. Se l'utente clicca sul titolo di un libro viene fatta un'altra richiesta API a openlibrary sul contenuto della descrizione
-5. Se la richiesta va a buon fine viene mostrata a schermo la descrizione
-
-*/
-
